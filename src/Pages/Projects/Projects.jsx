50596badf7ca51df1a9d934b22157d8a4cfb9014
ayaFarh <farhaya004@gmail.com>
2024-11-04 20:10:15 +0200

    un swiper

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 
-// import required modules
-import { EffectCoverflow, Pagination } from 'swiper/modules';
 
 export default function projects() {
     const projects = [
